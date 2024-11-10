41a09bf270568b8f7004360894db5fda69a2e34e
Zhengwei Zhang <51481762+ZhangZwaa@users.noreply.github.com>
2024-11-10 21:07:39 +0000

    commit6

--- stats_analysis.py
+++ stats_analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import statsmodels.api as sm
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -8,7 +9,6 @@ from statsmodels.regression.mixed_linear_model import MixedLM
 from scipy.stats import f_oneway
 import seaborn as sns
 import matplotlib.pyplot as plt
-from scipy.stats import pearsonr
 
 
 # Load data
@@ -52,22 +52,46 @@ print(f'\nRMSE for Baseline Model: {rmse:.2f}')
 
 
 # Analysis costs
-# Using one way ANOVA
-groups = [group['costs'] for _, group in data.groupby('insurance_type')]
+# Perform one-way ANOVA
+groups = [group['costs'].values for _, group in data.groupby('insurance_type')]
 f_stat, p_value = f_oneway(*groups)
 
-print(f"F-statistic: {f_stat:.2f}")
-print(f"P-value: {p_value}")
+# Print F-statistic and p-value
+print(f"F-statistic: {f_stat:.2f}") #
+print(f"P-value: {p_value:.4f}") #
 
+# Boxplot for visualizing costs by insurance type
 plt.figure(figsize=(10, 6))
-sns.boxplot(x="insurance_type",
-                y="costs",
-                data=data,
-                order=['Basic', 'Premium', 'Platinum', 'NoInsurance'])
-plt.xlabel("Insurance type")
-plt.ylabel("Costs per visit")
-plt.savefig('boxplot.png', format='png')
-# Basic statistics
-stats = data.groupby("insurance_type")["costs"].describe()
+sns.boxplot(
+    x="insurance_type",
+    y="costs",
+    data=data,
+    order=['Basic', 'Premium', 'Platinum', 'NoInsurance']
+)
+plt.xlabel("Insurance Type")
+plt.ylabel("Costs per Visit")
+plt.title("Costs per Visit by Insurance Type")
+plt.savefig('boxplot.png', format='png') #
+plt.show()
 
-corr, _ = pearsonr(data["insurance_type"], data["costs"])
+# Calculate basic statistics
+stats = data.groupby("insurance_type")["costs"].describe()
+print(stats) #
+
+# Calculate SS_between and SS_total for eta-squared
+grand_mean = data['costs'].mean()
+ss_total = np.sum((data['costs'] - grand_mean) ** 2)
+ss_between = sum([len(group) * (group.mean() - grand_mean) ** 2 for group in groups])
+
+# Calculate eta-squared
+eta_squared = ss_between / ss_total
+
+# Print effect size
+print(f"Eta-squared: {eta_squared:.4f}\n\n") #
+
+# Advanced analysis
+# Add confounder: visit month from visitdate
+data["visit_date"] = pd.to_datetime(data["visit_date"], format="%Y-%m-%d")
+data["visit_month"] = data["visit_date"].dt.month
+model_mul = sm.OLS.from_formula('walking_speed ~ age * education_level + visit_month', data).fit()
+print(model_mul.summary())
